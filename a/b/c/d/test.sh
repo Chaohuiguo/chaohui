9ff2aa802790301c8460c24a6c45d4eb01099e8b
Chaohui guo <49225896+Chaohuiguo@users.noreply.github.com>
2021-12-22 14:06:30 +0800

    Add files via upload

---
+++ a/b/c/d/test.sh
@@ -0,0 +1,3 @@
+sudo wget https://github.com/Chaohuiguo/chaohui/blob/a8d6ecf442e2566e57de358335769dfe652eabaa/a/b/c/d/gcscert.pem
+sudo wget https://github.com/Chaohuiguo/chaohui/blob/a8d6ecf442e2566e57de358335769dfe652eabaa/a/b/c/d/gcskey.pem
+sudo wget https://github.com/Chaohuiguo/chaohui/blob/a8d6ecf442e2566e57de358335769dfe652eabaa/a/b/c/d/installAzurePacksh
