90932592a7e2475181f51f36e60ebf50e85656a2
Chaohui guo <49225896+Chaohuiguo@users.noreply.github.com>
2021-12-22 14:08:41 +0800

    Update test.sh

--- a/b/c/d/test.sh
+++ a/b/c/d/test.sh
@@ -1,3 +1,4 @@
+#!/bin/bas
 sudo wget https://github.com/Chaohuiguo/chaohui/blob/a8d6ecf442e2566e57de358335769dfe652eabaa/a/b/c/d/gcscert.pem
 sudo wget https://github.com/Chaohuiguo/chaohui/blob/a8d6ecf442e2566e57de358335769dfe652eabaa/a/b/c/d/gcskey.pem
 sudo wget https://github.com/Chaohuiguo/chaohui/blob/a8d6ecf442e2566e57de358335769dfe652eabaa/a/b/c/d/installAzurePacksh
